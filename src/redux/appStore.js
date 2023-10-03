7a9b4c4ad5f6c0a5f5017264a69517f486c177e4
mohd-mustufa <mohammedmustufa02@gmail.com>
2023-10-04 01:31:23 +0530

    Created movie slice and added to the redux store

--- src/redux/appStore.js
+++ src/redux/appStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../redux/userSlice";
+import moviesReducer from "../redux/moviesSlice";
 
 const appStore = configureStore({
   reducer: {
     user: userReducer,
+    movies: moviesReducer,
   },
 });
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,8 +4,20 @@ export const LOGO_URL =
 export const BACKGROUND_URL_SMALL =
   "https://assets.nflxext.com/ffe/siteui/vlv3/9db4a880-3034-4e98-bdea-5d983e86bf52/b5953637-091d-4e02-9754-2bfadc8a8f7c/IN-en-20230925-popsignuptwoweeks-perspective_alpha_website_small.jpg";
 
+export const BACKGROUND_URL_LARGE =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/9db4a880-3034-4e98-bdea-5d983e86bf52/b5953637-091d-4e02-9754-2bfadc8a8f7c/IN-en-20230925-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+
 export const DEFAULT_AVATAR =
   "https://occ-0-1492-1009.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABdpkabKqQAxyWzo6QW_ZnPz1IZLqlmNfK-t4L1VIeV1DY00JhLo_LMVFp936keDxj-V5UELAVJrU--iUUY2MaDxQSSO-0qw.png?r=e6e";
 
-export const BACKGROUND_URL_LARGE =
-  "https://assets.nflxext.com/ffe/siteui/vlv3/9db4a880-3034-4e98-bdea-5d983e86bf52/b5953637-091d-4e02-9754-2bfadc8a8f7c/IN-en-20230925-popsignuptwoweeks-perspective_alpha_website_large.jpg";
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMmQ0MzM3Y2E0ZTlhZGFmOTMzMzM0ODI2YzIzY2JjYiIsInN1YiI6IjY1MWFmMmMwOTNiZDY5MDBmZTRhMjI2ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tKNw3Je8Mq_feekwOxvR9QGaxiZZRd4mluy2hVn2Zgo",
+  },
+};
+
+export const NOW_PLAYING_MOVIES_URL =
+  "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1";
